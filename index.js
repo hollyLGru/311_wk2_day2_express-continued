ca305d694c7a64fe722a3fc9eb3ba020f6a4502e
HollyLgru <H.grudovich@zollege.com>
2022-06-09 15:13:54 -0500

    added some comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const contacts  = require("./routes/contacts");
 const vehicles  = require("./routes/vehicles");
 const comments  = require("./routes/comments");
 const products  = require("./routes/products");
+// creates connections to the routes js files
 
 const app = express();
 
@@ -17,7 +18,8 @@ app.use(comments);
 app.use(contacts);
 app.use(products);
 app.use(vehicles);
-
+// These are using the code written in the controller js files so that comments, contacts,
+// products and vehicles can be added, deleted, changed and found 
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
--- routes/comments.js
+++ routes/comments.js
@@ -4,8 +4,11 @@ const router = express.Router();
 
 const commentsController = require('../controllers/comments')
 router.get("/comments");
+// shows all comments
 router.get("/comments", commentsController.list);
+
 router.get("/comments/:id", commentsController.show);
+// shows comments by ID
 router.post("/comments", commentsController.create);
 // POST route to create a new comment 
 
